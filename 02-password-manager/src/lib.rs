6b56c6f9b92223012265a4966978f7cdf52ca95f
Arturo Hernández <arturoaureliohr@gmail.com>
2025-08-31 23:15:10 -0400

    feat: add vault file reset prompt when initializing when the data file already exists

--- 02-password-manager/src/lib.rs
+++ 02-password-manager/src/lib.rs
@@ -7,55 +7,73 @@ use dialoguer::Confirm;
 use serde_json::Value;
 
 pub struct PasswordManager {
-    database_file_path: String,
+    vault_file_path: String,
+    vault_data: Option<Value>,
 }
 
 impl PasswordManager {
     pub fn new() -> PasswordManager {
-        let database_file_path =
-            env::var("PASSWORD_MANAGER_DATABASE_FILE_PATH").unwrap_or("./database.json".into());
+        let vault_file_path =
+            env::var("PASSWORD_MANAGER_VAULT_FILE_PATH").unwrap_or("./vault.json".into());
 
         PasswordManager {
-            database_file_path: database_file_path,
+            vault_file_path,
+            vault_data: None,
         }
     }
 
-    fn get_database_data(&self) -> Result<Value, io::Error> {
-        let database_raw = fs::read_to_string(&self.database_file_path)?;
+    fn get_vault_data(&self) -> Result<Value, io::Error> {
+        if let Some(vault_data) = &self.vault_data {
+            return Ok(vault_data.clone());
+        }
 
-        let database = serde_json::from_str(&database_raw)?;
+        let vault_data_raw = fs::read_to_string(&self.vault_file_path)?;
 
-        Ok(database)
+        let vault_data = serde_json::from_str(&vault_data_raw)?;
+
+        // self.vault_data = vault_data;
+
+        Ok(vault_data)
+    }
+
+    // fn get_derived_key(&self, master_password: &str) -> Result<(), Box<dyn Error>> {}
+
+    fn reset_vault(&self) -> Result<(), io::Error> {
+        fs::remove_file(&self.vault_file_path)?;
+
+        Ok({})
+    }
+
+    fn prompt_reset_vault(&self, message: &str) -> Result<(), Box<dyn Error>> {
+        let data_reset_confirmation = Confirm::new().with_prompt(message).interact()?;
+
+        if data_reset_confirmation {
+            self.reset_vault()?
+        }
+
+        Ok({})
     }
 
     pub fn initialize(&self) -> Result<(), Box<dyn Error>> {
-        println!("Verifying if database already exists");
-        let get_database_data_result = self.get_database_data();
+        println!("Verifying if vault file already exists");
+        let get_vault_data_result = self.get_vault_data();
 
-        match get_database_data_result {
+        match get_vault_data_result {
             Ok(_database) => {
-                return Err("Database already exists".into());
+                self.prompt_reset_vault(
+                    "Your vault file already exists, do you wish to reset it?",
+                )?;
             }
             Err(error) if error.kind() == io::ErrorKind::NotFound => {
                 // File is absent, we continue.
             }
             Err(error) if error.kind() == io::ErrorKind::InvalidData => {
-                let database_reset_confirmation = Confirm::new()
-                    .with_prompt(
-                        "Database seems to be  corrupted, do you wish to recreate your database file?",
-                    )
-                    .interact()?;
-
-                if database_reset_confirmation {
-                    fs::remove_file(&self.database_file_path)?;
-                } else {
-                    return Ok({});
-                }
+                self.prompt_reset_vault("Your vault file is corrupted, do you wish to reset it?")?;
             }
             Err(e) => return Err(e.into()),
         }
 
-        println!("Proceeding with database creation");
+        println!("Proceeding with initialization");
 
         Ok({})
     }
